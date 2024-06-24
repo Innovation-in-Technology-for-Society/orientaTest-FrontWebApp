812941469b1d0b8feb98d286c399c556424f7344
Jair Rodriguez <jair1232@hotmail.com>
2024-06-24 16:58:05 -0500

    docs:crear modulo universidades

---
+++ orienta-test/src/app/universidades/universidad/universidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-universidad',
+  templateUrl: './universidad.component.html',
+  styleUrl: './universidad.component.css'
+})
+export class UniversidadComponent {
+
+}
---
+++ orienta-test/src/app/universidades/universidades.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UniversidadComponent } from './universidad/universidad.component';
+
+
+
+@NgModule({
+  declarations: [
+    UniversidadComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UniversidadesModule { }
