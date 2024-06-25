93d1bf4dc13b167867e0787ac6ef41cf3274ab2e
Mauro <mauroadauto9@gmail.com>
2024-06-24 19:40:01 -0500

    feat: añadir path al modulo Resultados

--- orienta-test/src/app/app-routing.module.ts
+++ orienta-test/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent}
+  { path: '', redirectTo: '/resultados', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'resultados', loadChildren: () => import('./resultados/resultados.module').then(m => m.ResultadosModule) },
 ];
 
 @NgModule({
