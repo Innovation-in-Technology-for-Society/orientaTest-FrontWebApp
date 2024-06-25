88b0b9da72c587e9f5477914c48f0532f1ce0b5f
Jair Rodriguez <jair1232@hotmail.com>
2024-06-25 16:09:38 -0500

    dumb commit

--- orienta-test/src/app/app-routing.module.ts
+++ orienta-test/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'universidades', loadChildren: () => import('./universidades/universidades.module').then(m => m.UniversidadesModule) }, 
+  { path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
