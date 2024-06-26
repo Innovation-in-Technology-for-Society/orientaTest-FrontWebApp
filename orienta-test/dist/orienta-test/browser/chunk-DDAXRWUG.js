5063d4464045c0129fa3a8c0617fbc88a3d37951
Mauro <mauroadauto9@gmail.com>
2024-06-26 18:48:56 -0500

    deployment Front

---
+++ orienta-test/dist/orienta-test/browser/chunk-DDAXRWUG.js
@@ -0,0 +1,1 @@
+var t={producction:!1,apiUrl:"https://orientatest-api-restful.onrender.com/api/v1"};export{t as a};
---
+++ orienta-test/dist/orienta-test/browser/chunk-ZGOVKG76.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-KJCEJGLK.js";import"./chunk-DDAXRWUG.js";import"./chunk-YHAQZ7MM.js";import"./chunk-MZIQDI5T.js";export{a as ResultadosModule};
