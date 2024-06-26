e20e552d12ab7fb73c24c51a9ef0a7b13e44ca4c
David Niño <daavidninoosuaarez@gmail.com>
2024-06-26 07:32:48 -0500

    feat: actualizar rutas principales

--- orienta-test/src/app/app-routing.module.ts
+++ orienta-test/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VocacionalComponent } from './test/vocacional/vocacional.component';
+import { AutopercepcionComponent } from './test/autopercepcion/autopercepcion.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }, 
   { path: 'home', component: HomeComponent},
+  { path: 'test/vocacional', component: VocacionalComponent },
+  { path: 'test/autopercepcion', component: AutopercepcionComponent }
 ];
 
 @NgModule({
