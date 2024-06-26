05014e090ba5a1165400fa177fbd3901be8a06c9
David Niño <daavidninoosuaarez@gmail.com>
2024-06-26 07:33:54 -0500

    feat: añadir servicio Test

--- orienta-test/src/app/app.module.ts
+++ orienta-test/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TestModule } from './test/test.module';
 
+import { AuthInterceptor } from './user/interceptor/auth.interceptor';
+import { AuthService } from './user/services/auth.service';
+import { TestService } from './test/services/test.service';
+import { UserService } from './user/services/user.service';
 
 @NgModule({
   declarations: [
@@ -22,10 +27,19 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestModule
   ],
   providers: [
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    AuthService,
+    TestService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ],
   bootstrap: [AppComponent]
 })
