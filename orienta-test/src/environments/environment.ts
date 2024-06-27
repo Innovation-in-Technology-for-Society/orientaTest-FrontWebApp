c522e45a9f78dea7368a9dcbfa41063489d35a3b
Mauro <mauroadauto9@gmail.com>
2024-06-26 19:00:24 -0500

    deployment Front

--- orienta-test/src/environments/environment.ts
+++ orienta-test/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment={
     producction: false,
     //apiUrl: 'http://localhost:3000/api/v1'
-    apiUrl: 'http://localhost:8080/api/v1'
+    //apiUrl: 'http://localhost:8080/api/v1'
+    apiUrl: 'https://orientatest-api-restful.onrender.com/api/v1'
 }
